649bf62575d83000d08c75b486a138b6f8f2fd98
koh-gt <101822992+koh-gt@users.noreply.github.com>
2023-02-05 04:26:23 +0800

    Update guiconstants.h

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -42,12 +42,12 @@ static const int TOOLTIP_WRAP_THRESHOLD = 80;
 /* Number of frames in spinner animation */
 #define SPINNER_FRAMES 36
 
-#define QAPP_ORG_NAME "Litecoin"
-#define QAPP_ORG_DOMAIN "litecoin.org"
-#define QAPP_APP_NAME_DEFAULT "Litecoin-Qt"
-#define QAPP_APP_NAME_TESTNET "Liteocin-Qt-testnet"
-#define QAPP_APP_NAME_SIGNET "Litecoin-Qt-signet"
-#define QAPP_APP_NAME_REGTEST "Litecoin-Qt-regtest"
+#define QAPP_ORG_NAME "Ferrite"
+#define QAPP_ORG_DOMAIN "www.ferritecoin.org"
+#define QAPP_APP_NAME_DEFAULT "Ferrite-Qt"
+#define QAPP_APP_NAME_TESTNET "Ferrite-Qt-testnet"
+#define QAPP_APP_NAME_SIGNET "Ferrite-Qt-signet"
+#define QAPP_APP_NAME_REGTEST "Ferrite-Qt-regtest"
 
 /* One gigabyte (GB) in bytes */
 static constexpr uint64_t GB_BYTES{1000000000};
