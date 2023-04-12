ce086c2a3292451422eb8d95563f259be0d91927
koh-gt <101822992+koh-gt@users.noreply.github.com>
2023-04-12 22:04:53 +0800

    Update params.h

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -88,6 +88,8 @@ struct Params {
     bool fPowNoRetargeting;
     int64_t nPowTargetSpacing;
     int64_t nPowTargetTimespan;
+    int nPowKGWHeight;
+    int nPowDGWHeight;
     int64_t DifficultyAdjustmentInterval() const { return nPowTargetTimespan / nPowTargetSpacing; }
     /** The best chain should have at least this much work */
     uint256 nMinimumChainWork;
