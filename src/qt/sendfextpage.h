398f69a656de16f164e98929b73aadcc08da73f7
koh-gt <101822992+koh-gt@users.noreply.github.com>
2023-08-24 03:59:17 +0800

    Create sendfextpage.h

---
+++ src/qt/sendfextpage.h
@@ -0,0 +1,38 @@
+#ifndef SENDFEXTPAGE_H
+#define SENDFEXTPAGE_H
+
+#include <qt/platformstyle.h>
+
+#include <QWidget>
+#include <optional>
+
+class WalletModel;
+
+namespace Ui {
+    class SendFextPage;
+}
+
+QT_BEGIN_NAMESPACE
+QT_END_NAMESPACE
+
+/** Page for sending ferritext messages */
+class SendFextPage : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SendFextPage(const PlatformStyle *platformStyle, QWidget *parent = nullptr);
+    ~SendFextPage();
+
+    void setModel(WalletModel *walletModel);
+
+private:
+    const PlatformStyle *platformStyle;
+    Ui::SendFextPage *ui;
+    WalletModel *walletModel;
+
+private Q_SLOTS:
+    void onSendFextAction();
+};
+
+#endif
