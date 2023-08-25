b0345df0e356a49ccdab779d35e22aee9a207264
koh-gt <101822992+koh-gt@users.noreply.github.com>
2023-08-26 04:16:35 +0800

    Update sendfextpage.cpp

--- src/qt/sendfextpage.cpp
+++ src/qt/sendfextpage.cpp
@@ -61,92 +61,29 @@ void SendFextPage::onSendFextAction()
     if (!ctx.isValid())
         return;
   
-    //////////////////////////////////////////////////////////////////////
     // Qstring
-    const std::string strName = fextdata.toStdString();
-    LogPrint(BCLog::QT, "Ferrite Core sending text:%s\n", strName);
-    // Convert to Hex
-    std::string hexStrName = HexStr(strName);
-    LogPrint(BCLog::QT, "Ferrite Core sending hex:%s\n", hexStrName);
+    const std::string strfextdata = fextdata.toStdString();
+    std::string hexstrfextdata = HexStr(strfextdata);
+    LogPrint(BCLog::QT, "Ferrite Core sending hex:%s\n", hexstrfextdata);
 
-    // Create empty UniValue inputs array
-    UniValue inputs(UniValue::VARR);
-    
-    // Construct the outputs array with data output
-    UniValue outputs(UniValue::VARR);
-    UniValue dataOutput(UniValue::VOBJ);
-    dataOutput.pushKV("data", hexStrName);  // Replace with your desired data
-    outputs.push_back(dataOutput);
-    
-    // for the RPC call
-    UniValue params(UniValue::VARR);
-    params.push_back(inputs);
-    params.push_back(outputs);
-    
-    // Make the RPC call using executeRpc method
     std::string walletURI = "/wallet/" + walletModel->getWalletName().toStdString();
-    UniValue result;
-    bool success = walletModel->node().executeRpc("createrawtransaction", params, result, walletURI);
+
+    UniValue params(UniValue::VOBJ);
+    params.pushKV ("data", hexstrfextdata);
+    UniValue hexstringout = walletModel->node().executeRpc("createrawtransaction", params, walletURI);
     
-    // Check if the RPC call was successful
-    if (success) {
-        std::string rawTransaction = result.get_str();
-    } else {
-        std::string error = result.write();
-        return;
-    }
-    //////////////////////////////////////////////////////////////////////
-    // Construct the parameters array for the RPC call
-    UniValue params(UniValue::VARR);
-    params.push_back(rawTransaction);
+    UniValue params2(UniValue::VOBJ);
+    params2.pushKV ("hexstring", hexstringout);
+    UniValue hexstringout2 = walletModel->node().executeRpc("fundrawtransaction", params2, walletURI);
+
+    UniValue params3(UniValue::VOBJ);
+    params3.pushKV ("hexstring", hexstringout2);
+    UniValue hexstringout3 = walletModel->node().executeRpc("signrawtransactionwithwallet", params3, walletURI);
     
-    // Make the RPC call using executeRpc method
-    UniValue result;
-    bool success = walletModel->node().executeRpc("fundrawtransaction", params, result);
+    UniValue params4(UniValue::VOBJ);
+    params4.pushKV ("hexstring", hexstringout3);
+    walletModel->node().executeRpc("sendrawtransaction", params4, walletURI);
     
-    // Check if the RPC call was successful
-    if (success) {
-        std::string fundedRawTransaction = result["hex"].get_str();
-    } else {
-        std::string error = result.write();
-        return;
-    }
-    //////////////////////////////////////////////////////////////////////    
-    // Construct the parameters array for the RPC call
-    UniValue params(UniValue::VARR);
-    params.push_back(fundedRawTransaction);
-    
-    // Make the RPC call using executeRpc method
-    UniValue result;
-    bool success = walletModel->node().executeRpc("signrawtransactionwithwallet", params, result);
-    
-    // Check if the RPC call was successful
-    if (success) {
-        std::string signedRawTransaction = result["hex"].get_str();
-        bool complete = result["complete"].get_bool();     // You can also check the 'complete' flag to see if signing is complete       
-        // hex-encoded signed raw transaction   
-    } else {
-        std::string error = result.write();
-    }
-    //////////////////////////////////////////////////////////////////////
-    // Construct the parameters array for the RPC call
-    UniValue params(UniValue::VARR);
-    params.push_back(signedRawTransaction);
-    
-    // Make the RPC call using executeRpc method
-    UniValue result;
-    bool success = walletModel->node().executeRpc("sendrawtransaction", params, result);
-    
-    // Check if the RPC call was successful
-    if (success) {
-        std::string transactionHash = result.get_str();
-        // transaction hash of the broadcasted transaction
-    } else {
-        std::string error = result.write();
-        return;
-    }
-    //////////////////////////////////////////////////////////////////////
-  
     // reset UI text
     ui->sendFext->setText("");
     ui->sendFextButton->setDefault(true);
