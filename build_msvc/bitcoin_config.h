abce1fef244de4ad81b42741dd57680610b82dd0
koh-gt <101822992+koh-gt@users.noreply.github.com>
2024-06-03 06:47:17 +0800

    Update bitcoin_config.h

--- build_msvc/bitcoin_config.h
+++ build_msvc/bitcoin_config.h
@@ -15,25 +15,25 @@
 #define CLIENT_VERSION_IS_RELEASE true
 
 /* Major version */
-#define CLIENT_VERSION_MAJOR 0
+#define CLIENT_VERSION_MAJOR 3
 
 /* Minor version */
-#define CLIENT_VERSION_MINOR 21
+#define CLIENT_VERSION_MINOR 2
 
 /* Build revision */
-#define CLIENT_VERSION_REVISION 1
+#define CLIENT_VERSION_REVISION 0
 
 /* Copyright holder(s) before %s replacement */
 #define COPYRIGHT_HOLDERS "The %s developers"
 
 /* Copyright holder(s) */
-#define COPYRIGHT_HOLDERS_FINAL "The Bitcoin Core developers"
+#define COPYRIGHT_HOLDERS_FINAL "The Ferrite Core developers"
 
 /* Replacement for %s in copyright holders string */
-#define COPYRIGHT_HOLDERS_SUBSTITUTION "Bitcoin Core"
+#define COPYRIGHT_HOLDERS_SUBSTITUTION "Ferrite Core"
 
 /* Copyright year */
-#define COPYRIGHT_YEAR 2019
+#define COPYRIGHT_YEAR 2024
 
 /* Define to 1 to enable wallet functions */
 #define ENABLE_WALLET 1
@@ -247,22 +247,22 @@
 #define LT_OBJDIR ".libs/"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "https://github.com/bitcoin/bitcoin/issues"
+#define PACKAGE_BUGREPORT "https://github.com/ferritecoin/ferrite-core/issues"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "Bitcoin Core"
+#define PACKAGE_NAME "Ferrite Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Bitcoin Core 0.21.1"
+#define PACKAGE_STRING "Ferrite Core 3.2.0"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "bitcoin"
+#define PACKAGE_TARNAME "ferrite"
 
 /* Define to the home page for this package. */
-#define PACKAGE_URL "https://bitcoincore.org/"
+#define PACKAGE_URL "https://ferritecoin.org/"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.21.1"
+#define PACKAGE_VERSION "3.2.0"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
