0576849fd695019f5793a3ca4bee4fdf45dbc93f
koh-gt <101822992+koh-gt@users.noreply.github.com>
2026-06-23 23:34:30 +0800

    Update TxOutput.h

--- src/libmw/test/framework/include/test_framework/models/TxOutput.h
+++ src/libmw/test/framework/include/test_framework/models/TxOutput.h
@@ -4,15 +4,20 @@
 #include <mw/models/tx/Output.h>
 #include <mw/models/wallet/StealthAddress.h>
 #include <mw/crypto/Bulletproofs.h>
+#include <mw/crypto/Hasher.h>
 #include <mw/crypto/Pedersen.h>
+#include <mw/crypto/SecretKeys.h>
 
 TEST_NAMESPACE
 
 class TxOutput
 {
 public:
-    TxOutput(BlindingFactor&& blindingFactor, const uint64_t amount, Output&& output)
-        : m_blindingFactor(std::move(blindingFactor)), m_amount(amount), m_output(std::move(output)) { }
+    TxOutput(BlindingFactor&& blindingFactor, const uint64_t amount, Output&& output, SecretKey spend_key = {})
+        : m_blindingFactor(std::move(blindingFactor)),
+          m_amount(amount),
+          m_output(std::move(output)),
+          m_spendKey(std::move(spend_key)) { }
 
     static TxOutput Create(
         const SecretKey& sender_privkey,
@@ -25,8 +30,31 @@ public:
 
         return TxOutput{std::move(blind_switch), amount, std::move(output)};
     }
+    
+    static TxOutput Create(
+        const SecretKey& sender_privkey,
+        const SecretKey& receiver_scan_key,
+        const SecretKey& receiver_spend_key,
+        const uint64_t amount)
+    {
+        StealthAddress receiver_addr(
+            PublicKey::From(receiver_spend_key).Mul(receiver_scan_key),
+            PublicKey::From(receiver_spend_key)
+        );
+        BlindingFactor raw_blind;
+        Output output = Output::Create(&raw_blind, sender_privkey, receiver_addr, amount);
+        BlindingFactor blind_switch = Pedersen::BlindSwitch(raw_blind, amount);
+        SecretKey t = SecretKey::FromHash(Hashed(EHashTag::DERIVE, output.Ke().Mul(receiver_scan_key)));
+        SecretKey output_key = SecretKeys::From(receiver_spend_key)
+            .Mul(SecretKey::FromHash(Hashed(EHashTag::OUT_KEY, t)))
+            .Total();
+
+        return TxOutput{std::move(blind_switch), amount, std::move(output), output_key};
+    }
+
 
     const BlindingFactor& GetBlind() const noexcept { return m_blindingFactor; }
+    const SecretKey& GetSpendKey() const noexcept { return m_spendKey; }
     uint64_t GetAmount() const noexcept { return m_amount; }
     const Output& GetOutput() const noexcept { return m_output; }
     const Commitment& GetCommitment() const noexcept { return m_output.GetCommitment(); }
@@ -36,6 +64,7 @@ private:
     BlindingFactor m_blindingFactor;
     uint64_t m_amount;
     Output m_output;
+    SecretKey m_spendKey;
 };
 
 END_NAMESPACE
