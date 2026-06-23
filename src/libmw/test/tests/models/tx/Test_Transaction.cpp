1c47f21489d3f32e92bc5e06a731e9164f9e4ce0
koh-gt <101822992+koh-gt@users.noreply.github.com>
2026-06-23 21:31:56 +0800

    Update Test_Transaction.cpp

--- src/libmw/test/tests/models/tx/Test_Transaction.cpp
+++ src/libmw/test/tests/models/tx/Test_Transaction.cpp
@@ -43,7 +43,9 @@ BOOST_AUTO_TEST_CASE(TxTransaction)
     // Getters
     //
     {
-        BOOST_REQUIRE(tx->GetTotalFee() == fee);
+        const auto total_fee = tx->GetTotalFee();
+        BOOST_REQUIRE(total_fee.has_value());
+        BOOST_REQUIRE(*total_fee == fee);
     }
 }
 
