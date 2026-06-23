09caf298354e844fa715a69ab6652f4b92bdb6f4
koh-gt <101822992+koh-gt@users.noreply.github.com>
2026-06-23 22:31:42 +0800

    Update Test_BlockValidator.cpp

--- src/libmw/test/tests/node/Test_BlockValidator.cpp
+++ src/libmw/test/tests/node/Test_BlockValidator.cpp
@@ -83,6 +83,32 @@ BOOST_AUTO_TEST_CASE(BlockValidator_Test_PeginMismatch)
         );
         BOOST_CHECK(!is_valid);
     }
+
+    {
+        test::Tx pegin_tx = test::Tx::CreatePegIn(5'000'000);
+        mw::Block::CPtr pBlock = miner.MineBlock(3, {pegin_tx}).GetBlock();
+        PegInCoin pegin_coin = pegin_tx.GetPegInCoin();
+
+        bool is_valid = BlockValidator::ValidateBlock(
+            pBlock,
+            std::vector<PegInCoin>{
+                pegin_coin,
+                pegin_coin,
+            },
+            std::vector<PegOutCoin>{}
+        );
+        BOOST_CHECK(!is_valid);
+
+        is_valid = BlockValidator::ValidateBlock(
+            pBlock,
+            std::vector<PegInCoin>{
+                pegin_coin,
+                PegInCoin{0, pegin_coin.GetKernelID()},
+            },
+            std::vector<PegOutCoin>{}
+        );
+        BOOST_CHECK(!is_valid);
+    }
 }
 
 BOOST_AUTO_TEST_CASE(BlockValidator_Test_PegoutMismatch)
